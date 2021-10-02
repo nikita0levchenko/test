5b4d23cb64b6ead848ec38e42389870e7a3f1cd7
Nikita <nikita0levchenko@gmail.com>
2021-10-03 02:59:31 +0300

    fix: add count of symbols tabulation

--- FileReader.scala
+++ FileReader.scala
@@ -1,7 +1,8 @@
 object FileReader extends App{
     import scala.io.Source
-    def readFile(path: String): Unit =
-     if(path.nonEmpty) Source.fromFile(path).getLines().toList.foreach(println) else Console.err.println("Please input the path")
-
+    def readFile(path: String): Unit = if(path.nonEmpty)Source.fromFile(path).getLines().toList.foreach(x => 
+    println(x.trim.length + " " * (Source.fromFile(path).getLines().toList.map(_.trim.length).max.toString.length
+     - x.trim.length.toString.length) + " | " + x.trim))
+    else Console.err.println("Please input path of file")
     readFile(args(0))
 }
